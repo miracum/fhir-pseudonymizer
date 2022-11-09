f26a717ddc498ed6965edbf51e5db4d27efd5002
chgl <chgl@users.noreply.github.com>
2022-11-09 18:14:58 +0100

    fix: return pseudonym if it can't be de-pseudonymized when using vfps

--- src/FhirPseudonymizer/Pseudonymization/Vfps/VfpsPseudonymServiceClient.cs
+++ src/FhirPseudonymizer/Pseudonymization/Vfps/VfpsPseudonymServiceClient.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Vfps.Protos;
 
 namespace FhirPseudonymizer.Pseudonymization.Vfps;
 
 public class VfpsPseudonymServiceClient : IPseudonymServiceClient
 {
-    public VfpsPseudonymServiceClient(PseudonymService.PseudonymServiceClient client)
+    private readonly ILogger<VfpsPseudonymServiceClient> logger;
+
+    public VfpsPseudonymServiceClient(ILogger<VfpsPseudonymServiceClient> logger, PseudonymService.PseudonymServiceClient client)
     {
         Client = client;
+        this.logger = logger;
     }
 
     private PseudonymService.PseudonymServiceClient Client { get; }
@@ -33,8 +38,15 @@ public class VfpsPseudonymServiceClient : IPseudonymServiceClient
             Namespace = domain,
         };
 
-        var response = await Client.GetAsync(request);
-
-        return response.Pseudonym.OriginalValue;
+        try
+        {
+            var response = await Client.GetAsync(request);
+            return response.Pseudonym.OriginalValue;
+        }
+        catch (Exception exc)
+        {
+            logger.LogWarning(exc, "Failed to de-pseudonymize. Returning pseudonymized value.", pseudonym);
+            return pseudonym;
+        }
     }
 }
