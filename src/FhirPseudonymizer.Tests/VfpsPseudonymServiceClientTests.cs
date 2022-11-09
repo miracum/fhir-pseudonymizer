df9474f2aae6f144633e5619f805d6eb2dd0b764
chgl <chgl@users.noreply.github.com>
2022-11-09 18:30:38 +0100

    chore: updated to dotnet 7 and fixed vfps de-pseudonymization bug (#62)
    * chore(deps): update all non-major dependencies
    
    * chore: updated to dotnet 7
    
    * chore(deps): bumped major versions
    
    * fix: return pseudonym if it can't be de-pseudonymized when using vfps
    
    * ci: fixed codeql and added scorecard badge
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/FhirPseudonymizer.Tests/VfpsPseudonymServiceClientTests.cs
+++ src/FhirPseudonymizer.Tests/VfpsPseudonymServiceClientTests.cs
@@ -4,6 +4,7 @@ using FakeItEasy;
 using FhirPseudonymizer.Pseudonymization.Vfps;
 using FluentAssertions;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using Vfps.Protos;
 using Xunit;
 
@@ -28,7 +29,7 @@ public class VfpsPseudonymServiceClientTests
         A.CallTo(() => client.CreateAsync(A<PseudonymServiceCreateRequest>._, null, null, default))
             .Returns(new AsyncUnaryCall<PseudonymServiceCreateResponse>(Task.FromResult(fakeResponse), null, null, null, null));
 
-        var sut = new VfpsPseudonymServiceClient(client);
+        var sut = new VfpsPseudonymServiceClient(A.Fake<ILogger<VfpsPseudonymServiceClient>>(), client);
 
         // Act
         var result = await sut.GetOrCreatePseudonymFor("test", "namespace");
@@ -55,10 +56,28 @@ public class VfpsPseudonymServiceClientTests
         A.CallTo(() => client.GetAsync(A<PseudonymServiceGetRequest>._, null, null, default))
             .Returns(new AsyncUnaryCall<PseudonymServiceGetResponse>(Task.FromResult(fakeResponse), null, null, null, null));
 
-        var sut = new VfpsPseudonymServiceClient(client);
+        var sut = new VfpsPseudonymServiceClient(A.Fake<ILogger<VfpsPseudonymServiceClient>>(), client);
 
         // Act
         var result = await sut.GetOriginalValueFor("not test", "namespace");
+
+        // Assert
+        result.Should().Be("test");
+    }
+
+    [Fact]
+    public async Task GetOriginalValueFor_WithNonExistingPseudonym_ShouldReturnPseudonymValueInsteadOfOriginal()
+    {
+        // Arrange
+        var client = A.Fake<PseudonymService.PseudonymServiceClient>();
+
+        A.CallTo(() => client.GetAsync(A<PseudonymServiceGetRequest>._, null, null, default))
+            .Throws(() => throw new RpcException(new Status(StatusCode.NotFound, "doesn't exist")));
+
+        var sut = new VfpsPseudonymServiceClient(A.Fake<ILogger<VfpsPseudonymServiceClient>>(), client);
+
+        // Act
+        var result = await sut.GetOriginalValueFor("test", "namespace");
 
         // Assert
         result.Should().Be("test");
