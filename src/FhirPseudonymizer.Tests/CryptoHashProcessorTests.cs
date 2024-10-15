54dae1e0101ea5bedf9f56e128b49108407dea3e
chgl <chgl@users.noreply.github.com>
2024-10-15 20:45:53 +0200

    feat: added support for truncating crypto-hashed values to a max length (#207)
    * feat: added support for truncating crypto-hashed values to a max length
    
    * utf8bom -> utf8 & NL

--- src/FhirPseudonymizer.Tests/CryptoHashProcessorTests.cs
+++ src/FhirPseudonymizer.Tests/CryptoHashProcessorTests.cs
@@ -13,17 +13,32 @@ public class CryptoHashProcessorTests
         yield return new object[]
         {
             new FhirString("12345"),
-            "098fe201710ca56e73dfb56cb0c610a66900add818c6d625b44b91eaafe79022"
+            "098fe201710ca56e73dfb56cb0c610a66900add818c6d625b44b91eaafe79022",
         };
         yield return new object[]
         {
             new ResourceReference("Patient/12345"),
-            "Patient/098fe201710ca56e73dfb56cb0c610a66900add818c6d625b44b91eaafe79022"
+            "Patient/098fe201710ca56e73dfb56cb0c610a66900add818c6d625b44b91eaafe79022",
         };
         yield return new object[]
         {
             new FhirUri("Patient/12345"),
-            "Patient/098fe201710ca56e73dfb56cb0c610a66900add818c6d625b44b91eaafe79022"
+            "Patient/098fe201710ca56e73dfb56cb0c610a66900add818c6d625b44b91eaafe79022",
+        };
+    }
+
+    public static IEnumerable<object[]> GetTruncatedProcessData()
+    {
+        yield return new object[] { new FhirString("12345"), "098fe201710ca56e73dfb56cb0c610a6" };
+        yield return new object[]
+        {
+            new ResourceReference("Patient/12345"),
+            "Patient/098fe201710ca56e73dfb56cb0c610a6",
+        };
+        yield return new object[]
+        {
+            new FhirUri("Patient/12345"),
+            "Patient/098fe201710ca56e73dfb56cb0c610a6",
         };
     }
 
@@ -40,6 +55,29 @@ public class CryptoHashProcessorTests
         }
 
         processor.Process(node);
+
+        node.Value.ToString().Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTruncatedProcessData))]
+    public void Process_WithTruncatedHashLengthSet_HashHasMaxLength(
+        DataType element,
+        string expected
+    )
+    {
+        var processor = new CryptoHashProcessor("test");
+
+        var node = ElementNode.FromElement(element.ToTypedElement());
+        while (!node.HasValue())
+        {
+            node = node.Children().CastElementNodes().First();
+        }
+
+        processor.Process(
+            node,
+            settings: new Dictionary<string, object>() { { "truncateToMaxLength", 32 } }
+        );
 
         node.Value.ToString().Should().Be(expected);
     }
